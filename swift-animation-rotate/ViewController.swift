59d86243777841dd696e349b45bfaf2fa710f777
hiro.nagami <hiro.nagami@gmail.com>
2015-06-07 22:21:38 +0900

    rotate only mode

--- swift-animation-rotate/ViewController.swift
+++ swift-animation-rotate/ViewController.swift
@@ -13,10 +13,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var largeButton: UIButton!
-    var smallButton: UIButton!
-    var deleteButton: UIButton!
-    var moveButton: UIButton!
+    var rotateButton: UIButton!
     
     var buttonsAreaView: UIView!
     var animationAreaView: UIView?
@@ -51,32 +48,14 @@ class ViewController: UIViewController {
     
     func configureButtons()
     {
-        largeButton = UIButton(frame: CGRectMake(2, 2, buttonWidth, buttonHeight))
-        largeButton.backgroundColor = .lightGrayColor()
-        largeButton.setTitle("Rotate+Large", forState: .Normal)
-        largeButton.addTarget(self, action: Selector("rotateLargeAction"), forControlEvents: UIControlEvents.TouchUpInside)
-        
-        smallButton = UIButton(frame: CGRectMake(largeButton.frame.maxX+2, 2, buttonWidth, buttonHeight))
-        smallButton.backgroundColor = .lightGrayColor()
-        smallButton.setTitle("Rotate+Small", forState: .Normal)
-        smallButton.addTarget(self, action: Selector("rotateSmallAction"), forControlEvents: UIControlEvents.TouchUpInside)
-        
-        deleteButton = UIButton(frame: CGRectMake(2, largeButton.frame.maxY+2, buttonWidth, buttonHeight))
-        deleteButton.backgroundColor = .lightGrayColor()
-        deleteButton.setTitle("Rotate+Delete", forState: .Normal)
-        deleteButton.addTarget(self, action: Selector("rotateDeleteAction"), forControlEvents: UIControlEvents.TouchUpInside)
-        
-        moveButton = UIButton(frame: CGRectMake(largeButton.frame.maxX+2, largeButton.frame.maxY+2, buttonWidth, buttonHeight))
-        moveButton.backgroundColor = .lightGrayColor()
-        moveButton.setTitle("Rotate+Move", forState: .Normal)
-        moveButton.addTarget(self, action: Selector("rotateMoveAction"), forControlEvents: UIControlEvents.TouchUpInside)
+        rotateButton = UIButton(frame: CGRectMake(2, 2, buttonsAreaWidth, buttonsAreaHeight))
+        rotateButton.backgroundColor = .lightGrayColor()
+        rotateButton.setTitle("Rotate", forState: .Normal)
+        rotateButton.addTarget(self, action: Selector("rotateAction"), forControlEvents: UIControlEvents.TouchUpInside)
         
         buttonsAreaView = UIView(frame: CGRectMake(0, buttonsAreaOriginY, buttonsAreaWidth, buttonsAreaHeight))
         
-        buttonsAreaView.addSubview(largeButton)
-        buttonsAreaView.addSubview(smallButton)
-        buttonsAreaView.addSubview(deleteButton)
-        buttonsAreaView.addSubview(moveButton)
+        buttonsAreaView.addSubview(rotateButton)
         
         view.addSubview(buttonsAreaView)
     }
@@ -109,94 +88,13 @@ class ViewController: UIViewController {
         return animationView
     }
     
-    func rotateLargeAction()
-    {
-        self.rotateAction()
-        self.largeAction()
-    }
-    
-    func rotateSmallAction()
-    {
-        self.rotateAction()
-        self.smallAction()
-    }
-    
-    func rotateDeleteAction()
-    {
-        self.rotateAction()
-        self.deleteAction()
-    }
-    
-    func rotateMoveAction()
-    {
-        self.rotateAction()
-        self.moveAction()
-    }
-    
     func rotateAction()
     {
         if let _animationView = animationView {
             var viewCenter = _animationView.center
             UIView.animateWithDuration(1.0, animations: { () -> Void in
                 _animationView.transform = CGAffineTransformMakeRotation(CGFloat(self.DegreesToRadians(180.0)))
-                })
-        }
-    }
-    
-    func largeAction()
-    {
-        if let _animationView = animationView {
-            var viewCenter = _animationView.center
-            UIView.animateWithDuration(1.0, animations: { () -> Void in
-                _animationView.frame.size.width = _animationView.frame.size.width * 2
-                _animationView.frame.size.height = _animationView.frame.size.height * 2
-                _animationView.center = viewCenter
-                }) { (isTrue) -> Void in
-                    self.configureAnimationView()
-            }
-        }
-    }
-    
-    func smallAction()
-    {
-        if let _animationView = animationView {
-            var viewCenter = _animationView.center
-            UIView.animateWithDuration(1.0, animations: { () -> Void in
-                _animationView.frame.size.width = _animationView.frame.size.width / 2
-                _animationView.frame.size.height = _animationView.frame.size.height / 2
-                _animationView.center = viewCenter
-                }) { (isTrue) -> Void in
-                    self.configureAnimationView()
-            }
-        }
-    }
-    
-    func deleteAction()
-    {
-        if animationView != nil {
-            var viewCenter = self.animationView!.center
-            UIView.animateWithDuration(1.0, animations: { () -> Void in
-                self.animationView!.alpha = 0
-                }) { (isTrue) -> Void in
-                    self.animationView?.removeFromSuperview()
-                    self.animationView = nil
-                    self.deleteButton.setTitle("Add", forState: .Normal)
-            }
-        }
-        else {
-            configureAnimationView()
-            self.deleteButton.setTitle("Delete", forState: .Normal)
-        }
-    }
-    
-    func moveAction()
-    {
-        if let _animationView = animationView {
-            var viewCenter = _animationView.center
-            _animationView.frame.origin.x = 0
-            UIView.animateWithDuration(1.0, animations: { () -> Void in
-                _animationView.frame.origin.x = self.view.frame.width - _animationView.frame.width
-                }) { (isTrue) -> Void in
+                }){ (isTrue) -> Void in
                     self.configureAnimationView()
             }
         }
